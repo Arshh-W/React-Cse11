ab5edb34d8ea96867e0daa1d3266711ca92094cb
Arsh <q43676709@gmail.com>
2026-04-10 12:19:32 +0530

    Creating a simple functional component

--- Ar/src/App.jsx
+++ Ar/src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import  Greeting from './Greeting.jsx'
 
 function App() {
 
 
   return (
     <>
-    <h1> Hello</h1>
+      <Greeting/>
     </>
   )
 }
